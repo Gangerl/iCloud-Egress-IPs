5dad4188c902c6dcd7e3e74d0db613b08dd1bb5e
Wolfgang <38921820+Gangerl@users.noreply.github.com>
2024-07-08 17:41:37 +0200

    Create icloud-egress-ips.sh

---
+++ icloud-egress-ips.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Apple's iCloud Egress IP-Ranges summarized by /16 for IPv4 and /32 for IPv6
+# Wolfgang Lauffher 2024
+
+curl --silent --fail https://mask-api.icloud.com/egress-ip-ranges.csv \
+| gawk -F "[:.]" '
+
+	/^[[:digit:]]+\./ {		# v4 has some digits "0-9" followed by "."
+		v4_16[$1 "." $2]++
+	}
+
+	/^[[:alnum:]]+:/ {		# v6 has some chars "0-9a-f" followed by ":"
+		v6_32[$1 ":" $2]++
+	}
+
+	END {
+		for (entry in v4_16) printf "%-10s has %7s rows\n", entry, v4_16[entry]
+		for (entry in v6_32) printf "%-10s has %7s rows\n", entry, v6_32[entry]
+	}
+'
+
+# vim: syntax=awk
